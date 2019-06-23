b3ebb6877f8fb7a0c2f5caaf0d9b93ad19578b53
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-06-23 17:38:15 +0900

    [update]管理者ログイン後の遷移先を商品一覧画面へ変更

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller {
 	 *
 	 * @var string
 	 */
-	protected $redirectTo = '/admin/home';
+	protected $redirectTo = '/admin/item';
 
 	/**
 	 * Create a new controller instance.
