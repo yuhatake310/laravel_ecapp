3d007b0d2ec640c54c1100fba04b3f347fe12c1a
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-06-23 11:59:47 +0900

    [update]モデルを作成し、シンプルなコードへ修正

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Item;
 
 class ItemController extends Controller {
 	public function index() {
-		$items = \DB::table('items')->get();
+		$items = Item::get();
 		return view('item.index', compact('items'));
 	}
 
 	public function detail($item_id) {
-		$item = \DB::table('items')->find($item_id);
+		$item = Item::find($item_id);
 		return view('item.detail', compact('item'));
 	}
 }
