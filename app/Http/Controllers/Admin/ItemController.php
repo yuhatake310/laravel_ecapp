e0a9068e44504e9fef2ad41e74d2534b7466b500
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-06-23 17:39:10 +0900

    [add]管理者用商品コントローラーを新規作成

---
+++ app/Http/Controllers/Admin/ItemController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Item;
+use App\Http\Controllers\Controller;
+
+class ItemController extends Controller {
+	public function __construct() {
+		$this->middleware('auth:admin');
+	}
+
+	public function index() {
+		$items = Item::get();
+		return view('admin.item.index', compact('items'));
+	}
+
+	public function detail($item_id) {
+		$item = Item::find($item_id);
+		return view('admin.item.detail', compact('item'));
+	}
+}
