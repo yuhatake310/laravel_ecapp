12fd1ca5dc9ed388dc853a54b9b47b668719adc6
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-06-24 16:52:57 +0900

    [update]商品追加リンクを加え、テーブルのレイアウトを調整

--- resources/views/admin/item/index.blade.php
+++ resources/views/admin/item/index.blade.php
@@ -8,26 +8,27 @@
 				<div class="panel-heading">商品一覧</div>
 
 				<div class="panel-body">
-					<table border="1">
-					<tr>
-					<th>商品名</th>
-					<th>価格</th>
-					<th>在庫</th>
-					</tr>
-					@foreach ($items as $item)
-						<tr align="center">
-						<td><a href="{{ route('admin.item.detail', ['item_id' => $item->id]) }}">{{ $item->name }}</a></td>
-						<td>{{ $item->price }}</td>
-						<td>
-						@if ($item->stock_quantity === 0)
-							在庫無し
-						@else
-							在庫あり
-						@endif
-						</td>
+					<table class="table table-bordered">
+						<tr>
+							<th>商品名</th>
+							<th>価格</th>
+							<th>在庫</th>
 						</tr>
-					@endforeach
+						@foreach ($items as $item)
+							<tr>
+								<td><a href="{{ route('admin.item.detail', ['item_id' => $item->id]) }}">{{ $item->name }}</a></td>
+								<td>{{ $item->price }}</td>
+								<td>
+								@if ($item->stock_quantity === 0)
+									在庫無し
+								@else
+									在庫あり
+								@endif
+								</td>
+							</tr>
+						@endforeach
 					</table>
+					<a href="{{ route('admin.item.add') }}">商品追加</a>
 				</div>
 			</div>
 		</div>
