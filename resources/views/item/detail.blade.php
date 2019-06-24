798d53ce78bba9dc38842198aadb3289ff6f3e5a
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-06-25 00:01:35 +0900

    [update]レイアウト調整

--- resources/views/item/detail.blade.php
+++ resources/views/item/detail.blade.php
@@ -1,20 +1,26 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-<title>商品詳細画面</title>
-</head>
-<body>
-<h2>商品詳細</h2>
-<p>商品名：{{ $item->name }}</p>
-<p>説明：{{ $item->description }}</p>
-<p>価格：{{ $item->price }}</p>
-<p>在庫：
-@if ($item->stock_quantity === 0)
-	在庫無し
-@else
-	在庫あり
-@endif
-</p>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">商品詳細</div>
+
+				<div class="panel-body">
+					<p>商品名：{{ $item->item_name }}</p>
+					<p>説明：{{ $item->description }}</p>
+					<p>価格：{{ $item->price }}</p>
+					<p>在庫：
+					@if ($item->stock_quantity === 0)
+						在庫無し
+					@else
+						在庫あり
+					@endif
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -1,30 +1,36 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-<title>商品一覧画面</title>
-</head>
-<body>
-<h2>商品一覧</h2>
-<table border="1">
-<tr>
-<th>商品名</th>
-<th>価格</th>
-<th>在庫</th>
-</tr>
-@foreach ($items as $item)
-	<tr align="center">
-	<td><a href="{{ route('item.detail', ['item_id' => $item->id]) }}">{{ $item->name }}</a></td>
-	<td>{{ $item->price }}</td>
-	<td>
-	@if ($item->stock_quantity === 0)
-		在庫無し
-	@else
-		在庫あり
-	@endif
-	</td>
-	</tr>
-@endforeach
-</table>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="panel panel-default">
+				<div class="panel-heading">商品一覧</div>
+
+				<div class="panel-body">
+					<table class="table table-bordered">
+						<tr>
+							<th>商品名</th>
+							<th>価格</th>
+							<th>在庫</th>
+						</tr>
+						@foreach ($items as $item)
+							<tr>
+								<td><a href="{{ route('item.detail', ['item_id' => $item->id]) }}">{{ $item->item_name }}</a></td>
+								<td>{{ $item->price }}</td>
+								<td>
+								@if ($item->stock_quantity === 0)
+									在庫無し
+								@else
+									在庫あり
+								@endif
+								</td>
+							</tr>
+						@endforeach
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
