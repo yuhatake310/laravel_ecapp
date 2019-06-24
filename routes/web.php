8db559355452aea2bd8c63bc1da37814151297ad
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-06-25 00:06:13 +0900

    [update]ユーザー側のルートを商品一覧へ変更

--- routes/web.php
+++ routes/web.php
@@ -11,26 +11,25 @@
 |
 */
 
+Auth::routes();
+
+Route::get('/', function () { return redirect('/item'); });
 Route::get('/item', 'ItemController@index');
 Route::get('/item/detail/{item_id}', 'ItemController@detail')->name('item.detail');
 
-Auth::routes();
-
-Route::get('/', function () { return redirect('/home'); });
-
 Route::group(['middleware' => 'auth:user'], function() {
-    Route::get('/home', 'HomeController@index')->name('home');
+	Route::get('/home', 'HomeController@index')->name('home');
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('/', function () { return redirect('/admin/item'); });
-    Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
-    Route::post('login', 'Admin\LoginController@login');
+	Route::get('/', function () { return redirect('/admin/item'); });
+	Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
+	Route::post('login', 'Admin\LoginController@login');
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
-    Route::post('logout', 'Admin\LoginController@logout')->name('admin.logout');
-    Route::get('home', 'Admin\HomeController@index')->name('admin.home');
+	Route::post('logout', 'Admin\LoginController@logout')->name('admin.logout');
+	Route::get('home', 'Admin\HomeController@index')->name('admin.home');
 	Route::get('item', 'Admin\ItemController@index')->name('admin.item.index');
 	Route::get('item/add', 'Admin\ItemController@showAddForm')->name('admin.item.add');
 	Route::post('item/add', 'Admin\ItemController@add');
