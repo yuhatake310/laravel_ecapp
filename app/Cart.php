7e175664a243b8fe966553d0b92e37136421bf67
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-07-02 18:00:34 +0900

    [update]カートテーブルのカラム保護を追加

--- app/Cart.php
+++ app/Cart.php
@@ -9,6 +9,10 @@ class Cart extends Model {
 	use SoftDeletes;
 	protected $dates = ['deleted_at'];
 
+	protected $fillable = [
+		'user_id', 'item_id', 'item_quantity',
+	];
+
 	public function user() {
 		return $this->belongsTo('App\User');
 	}
