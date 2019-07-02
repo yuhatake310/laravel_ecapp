5896d207c6bd7c255713588c5325eb9780fbfd5d
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-07-02 18:01:35 +0900

    [update]ログインと会員登録終了後の遷移先を商品一覧画面へ変更

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/item';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -27,7 +27,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/item';
 
     /**
      * Create a new controller instance.
