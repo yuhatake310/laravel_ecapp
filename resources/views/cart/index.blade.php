edb3745056eefc6b9e32fcd208d5fcb20628bfc2
yuhatake310 <45507104+yuhatake310@users.noreply.github.com>
2019-07-02 18:03:40 +0900

    [fix]カート一覧表示のコードの記載方法を修正
    複数レコードの時に、テーブルヘッダ行も繰り返されてしまっていたため

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -8,22 +8,26 @@
 				<div class="panel-heading">カート</div>
 
 				<div class="panel-body">
-					@forelse ($carts as $cart)
+					@if ($carts->isEmpty())
+						カートは空です
+					@else
 						<table class="table table-bordered">
 							<tr>
 								<th>商品名</th>
 								<th>価格</th>
+								<th>個数</th>
 								<th>アクション</th>
 							</tr>
+							@foreach ($carts as $cart)
 							<tr>
 								<td>{{ $cart->item->item_name }}</td>
 								<td>{{ $cart->item->price }}</td>
+								<td>{{ $cart->item_quantity }}</td>
 								<td><a href="{{ route('cart.delete', ['cart_id' => $cart->id]) }}">削除</a></td>
 							</tr>
-						</table>
-					@empty
-						カートが空です
-					@endforelse
+							@endforeach
+							</table>
+					@endif
 				</div>
 			</div>
 		</div>
